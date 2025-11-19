0659dd93b6a29f0c67bf45ff4413906196c514f6
ferryss <2028337982@qq.com>
2025-11-19 19:16:31 +0800

    增加了网关模块

---
+++ gateway/src/main/java/com/szx/train/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.szx.train.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ferry
+ * @date 2025/11/19
+ * @project train
+ * @description
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
