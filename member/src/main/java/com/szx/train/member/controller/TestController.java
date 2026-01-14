f281c953fe0dc7f4613eea388b16b7cd01f36d25
ferryss <2028337982@qq.com>
2026-01-14 18:00:41 +0800

    配置nacos注册中心和配置中心，gateway通过模块名转发（负载均衡）

--- member/src/main/java/com/szx/train/member/controller/TestController.java
+++ member/src/main/java/com/szx/train/member/controller/TestController.java
@@ -3,6 +3,8 @@ package com.szx.train.member.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping
+@RefreshScope
 public class TestController {
 
     private static final Logger log = LoggerFactory.getLogger(TestController.class);
+    @Value("${test.spring}")
+    private String test;
 
     @GetMapping("/test")
     public void test(HttpServletRequest  request){
         String header = request.getHeader("user-info");
         log.info("==========访问成功==========");
         log.info("用户信息：{}", header);
+        log.info("测试  {}", test);
     }
 }
